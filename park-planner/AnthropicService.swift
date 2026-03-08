d79c6838fd39c98ef646bebb011f83552a4c2cad
parkermorgan <156543039+parkermorgan@users.noreply.github.com>
2026-03-07 22:21:17 -0700

    Added AI integration, improved stying for all pages and added data clear option.

---
+++ park-planner/AnthropicService.swift
@@ -0,0 +1,8 @@
+//
+//  AnthropicService.swift
+//  park-planner
+//
+//  Created by Parker Morgan on 3/7/26.
+//
+
+import Foundation
---
+++ park-planner/HelperView.swift
@@ -0,0 +1,18 @@
+//
+//  HelperView.swift
+//  park-planner
+//
+//  Created by Parker Morgan on 3/7/26.
+//
+
+import SwiftUI
+
+struct HelperView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HelperView()
+}
