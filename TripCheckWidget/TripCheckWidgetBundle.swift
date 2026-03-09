99abc163ed146283d6e51dc4247447e1a2209c5e
parkermorgan <156543039+parkermorgan@users.noreply.github.com>
2026-03-08 22:19:49 -0600

    Added dark mode support and widget functionality.

---
+++ TripCheckWidget/TripCheckWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  TripCheckWidgetBundle.swift
+//  TripCheckWidget
+//
+//  Created by Parker Morgan on 3/8/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TripCheckWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TripCheckWidget()
+        TripCheckWidgetControl()
+        TripCheckWidgetLiveActivity()
+    }
+}
